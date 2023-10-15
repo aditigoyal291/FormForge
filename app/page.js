8632d251c99e4b6ab1addb881031db751dc3c49b
aditigoyal291 <goyaladiti2912@gmail.com>
2023-10-15 12:32:39 +0530

    Made the home page'

--- app/page.js
+++ app/page.js
@@ -1,14 +1,37 @@
 import Link from "next/link";
 import React from "react";
+import { AiOutlineRight } from "react-icons/ai";
 
 const page = () => {
   return (
-    <div className="text-white min-h-screen flex items-center justify-center">
-      <Link href="/registration">
-        <button className="border p-2 px-3 rounded border-white/50 hover:bg-white/10">
-          Register
-        </button>
-      </Link>
+    <div className="text-white min-h-screen max-phone:max-h-full flex items-center justify-center flex-col gap-4 ">
+      <div class=" flex items-center justify-center">
+        <div class=" w-full rounded-full bg-gradient-to-r from-green-700 via-green-500  via-yellow-500 via-orange-400 via-pink-400 to-indigo-800 p-[1px]">
+          <div class="flex h-full w-full items-center justify-center bg-background back gap-2  rounded-full py-1 px-3">
+            <p class="text-sm text-white font-normal">Exclusively for PESU</p>
+            <span>
+              <AiOutlineRight className="text-[8px]" />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex justify-center flex-col items-center gap-2 bg-gradient-to-br from-white text-transparent bg-clip-text font-semibold">
+        <span className="text-5xl">Forms For</span>
+        <p className="text-5xl">Developers</p>
+      </div>
+      <div className="flex flex-col gap-6 items-center  w-fit ">
+        <p className="text-text-primary  text-lg ">
+          The most reusable form you'll ever find xD
+        </p>
+        <Link href="/registration" className="w-full">
+          <button className="bg-foreground px-6  rounded-full w-full  text-black py-3 hover:bg-text-primary flex justify-center gap-2">
+            <p className="font-semibold ">Register</p>
+            <span>
+              <AiOutlineRight className="inline text-xs" />
+            </span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
