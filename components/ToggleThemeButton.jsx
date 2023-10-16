4f37bc1259f33dc2470ec330229d6452a76f7d3d
aditigoyal291 <goyaladiti2912@gmail.com>
2023-10-16 17:57:59 +0530

    Swapped the button in dark mode

--- components/ToggleThemeButton.jsx
+++ components/ToggleThemeButton.jsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
-import { useTheme } from 'next-themes';
-import React from 'react';
-import { BsSunFill, BsFillMoonFill } from 'react-icons/bs';
-import Button from './Button';
+import { useTheme } from "next-themes";
+import React from "react";
+import { BsSunFill, BsFillMoonFill } from "react-icons/bs";
+import Button from "./Button";
 
 const ToggleThemeButton = () => {
-	const { setTheme, theme } = useTheme();
-	return (
-		<Button
-			variant="ghost"
-			size="icon"
-			handleClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-		>
-			<BsSunFill className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-			<BsFillMoonFill className="hidden h-5 w-5 dark:block" />
-			<span className="sr-only">Toggle theme</span>
-		</Button>
-	);
+  const { setTheme, theme } = useTheme();
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      handleClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      <BsSunFill className="h-[1.5rem] w-[1.3rem] dark:block" />
+      <BsFillMoonFill className="hidden h-5 w-5 dark:hidden" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 };
 
 export default ToggleThemeButton;
